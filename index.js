d4f2ca1c125356b7267b0457f16af81df84458c1
Owner <129130014+hysteria222@users.noreply.github.com>
2024-09-22 12:59:57 -0500

    get req done

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // require your server and launch it here
-const server = require('./api/server')
+const server = require('./api/server');
 
-const PORT = 5000
+const PORT = 5000;
 
 server.listen(PORT, () => {
-    console.log('listening on ', PORT)
+  console.log('listening on ', PORT);
 });
